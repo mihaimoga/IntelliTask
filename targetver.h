8576f6aa5e09191dce830026ba5612a35508ccb9
Stefan-Mihai Moga <stefan-mihai.moga@nxp.com>
2023-02-18 15:07:17 +0200

    Updated copyright header

--- targetver.h
+++ targetver.h
@@ -1,3 +1,16 @@
+/* This file is part of IntelliTask application developed by Mihai MOGA.
+
+IntelliTask is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Open
+Source Initiative, either version 3 of the License, or any later version.
+
+IntelliTask is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+IntelliTask. If not, see <http://www.opensource.org/licenses/gpl-3.0.html>*/
+
 // This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface
 // (the "Fluent UI") and is provided only as referential material to supplement the
 // Microsoft Foundation Classes Reference and related electronic documentation
